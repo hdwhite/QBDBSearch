27269bb50de1a09b03dd4e9b312d2cc64cea51b2
Harry White <hdwhite@vt.edu>
2019-07-16 16:36:04 -0400

    Updated index text

--- content/indexbody.php
+++ content/indexbody.php
@@ -1,7 +1,7 @@
 <div id="content">
 	<div class="entry">
 		<h4>How to use</h4>
-		<p>This site searches the <a href="http://hsquizbowl.org/db">Quizbowl Resource Database</a> for all stat reports containing a given team or player. Searches are made "as is", and no effort is made to search for common alternate names (such as VT for Virginia Tech). The site is indexed on a more-or-less weekly basis, so recently-posted results may not show up right away. To search for one of multiple different terms, separate the commands with an "OR" (e.g. "VT or Virginia Tech" should show all results for said school).</p><br>
+		<p>This site searches the <a href="http://hsquizbowl.org/db">Quizbowl Resource Database</a>, <a href="https://stats.neg5.org/">Neg5</a>, and <a href="https://www.naqt.com/stats/tournament/">NAQT</a> for all stat reports containing a given team or player. Searches are made "as is", and no effort is made to search for common alternate names (such as VT for Virginia Tech). The site is indexed on a more-or-less weekly basis, so recently-posted results may not show up right away. To search for one of multiple different terms, separate the commands with an "OR" (e.g. "VT or Virginia Tech" should show all results for said school).</p><br>
 		<p>Currently, there are <?=number_format($numplayers) ?> individual player stats from <?=number_format($numteams) ?> teams in <?=number_format($numtourneys) ?> tournaments indexed on this site.</p>
 	</div>
 	<?php require("content/form.php"); ?>
